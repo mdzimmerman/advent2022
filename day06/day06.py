7313ed8f2cd0ed95f83aa5f97985c80b96b19a60
Matthew Zimmerman <matt.zimmerman@ccri.com>
2022-12-13 10:40:22 -0500

    day 6

--- day06/day06.py
+++ day06/day06.py
@@ -4,6 +4,22 @@ def readstring(filename):
         for l in fh:
             s += l.strip()
     return s
-    
+
+def sliding(xs, size):
+    for i in range(len(xs)-size+1):
+        yield xs[i:i+size]
+
+def find_marker(s, size):
+    for i, w in enumerate(sliding(s, size)):
+        #print(w)
+        if len(set(w)) == size:
+            return i+size
+
 if __name__ == '__main__':
-    print(readstring())
+    test = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    print(find_marker(test, 4))
+    print(find_marker(test, 14))
+
+    inp = readstring("input.txt")
+    print(find_marker(inp, 4))
+    print(find_marker(inp, 14))
