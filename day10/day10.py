248533abcba9af89289e5b727500d0b2de3364b3
Matthew Zimmerman <matt.zimmerman@ccri.com>
2022-12-22 20:02:13 -0500

    day 11

--- day10/day10.py
+++ day10/day10.py
@@ -8,16 +8,22 @@ class ClockCpu:
         self.clock = 0
         self.x = 1
         self.strength = 0
+        self.screen = []
         
     def clockinc(self):
         self.clock += 1
+        pixel = (self.clock-1)%40
+        if abs(pixel - self.x) <= 1:
+            self.screen.append('#')
+        else:
+            self.screen.append('.')
         if self.clock in [20, 60, 100, 140, 180, 220]:
             self.strength += self.clock * self.x
     
     def evaluate(self):
         self.reset()
         for instr in self.instructions:
-            print(instr)
+            #print(instr)
             if instr[0] == "noop":
                 self.clockinc()
             elif instr[0] == "addx":
@@ -25,6 +31,12 @@ class ClockCpu:
                 self.clockinc()
                 self.x += int(instr[1])
         return self.strength
+    
+    def print_screen(self):
+        for j in range(6):
+            for i in range(40):
+                print(self.screen[j*40+i], end="")
+            print()
     
     @classmethod
     def fromfile(cls, filename):
@@ -40,4 +52,5 @@ if __name__ == "__main__":
     #print(sys.argv)
     cpu = ClockCpu.fromfile(sys.argv[1])
     print(cpu.evaluate())
+    cpu.print_screen()
         
---
+++ day11/day11.py
@@ -0,0 +1,89 @@
+import argparse
+import re
+import sys
+
+if ".." not in sys.path:
+    sys.path.append("..")
+import aoc
+
+def read_pattern(pat, s):
+    m = pat.match(s)
+    if m:
+        return m.group(1)
+    else:
+        return None
+
+class Monkey:
+    pmonkey = re.compile(r"Monkey (\d):")
+    pstart  = re.compile(r"  Starting items: (.+)")
+    pexpr   = re.compile(r"  Operation: new = (.+)")
+    ptesti  = re.compile(r"  Test: divisible by (\d+)")
+    ptrue   = re.compile(r"    If true: throw to monkey (\d)")
+    pfalse  = re.compile(r"    If false: throw to monkey (\d)")
+    
+    def __init__(self, n, start, expr, testi, ntrue, nfalse):
+        self.n = n
+        self.start = start
+        self.expr = expr
+        self.testi = testi
+        self.ntrue = ntrue
+        self.nfalse = nfalse
+        
+    def __repr__(self):
+        return f"Monkey(n={self.n} start={self.start} expr={self.expr} testi={self.testi} ntrue={self.ntrue} nfalse={self.nfalse})"
+
+    def calcworry(self, x):
+        return eval(self.expr, {}, {"old": x})
+    
+    def throwto(self, x):
+        if x % self.testi == 0:
+            return self.ntrue
+        else:
+            return self.nfalse
+        
+    @classmethod
+    def fromtext(cls, xs):
+        n      = int(read_pattern(cls.pmonkey, xs[0]))
+        start  = [int(x) for x in read_pattern(cls.pstart, xs[1]).split(", ")]
+        expr   = read_pattern(cls.pexpr, xs[2])
+        testi  = int(read_pattern(cls.ptesti, xs[3]))
+        ntrue  = int(read_pattern(cls.ptrue, xs[4]))
+        nfalse = int(read_pattern(cls.pfalse, xs[5]))
+        return Monkey(n, start, expr, testi, ntrue, nfalse)
+
+def print_inventory(inventory):
+    for i in range(len(inventory)):
+        inv = ", ".join(str(x) for x in inventory[i])
+        print(f"{i}: {inv}")
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    
+    args = parser.parse_args()
+    monkeys = []
+    for xs in aoc.splitlist(aoc.readlines(args.filename), ""):
+        monkeys.append(Monkey.fromtext(xs))
+    #print(monkeys[0])
+    
+    inventory = []
+    inspectcount = []
+    for i, m in enumerate(monkeys):
+        inventory.append(m.start[:])
+        inspectcount.append(0)
+    print_inventory(inventory)
+    
+    rounds = 20
+    for _ in range(rounds):
+        for i, m in enumerate(monkeys):
+            for item in inventory[i]:
+                inspectcount[i] += 1
+                newitem = m.calcworry(item)
+                dest = m.throwto(newitem)
+                inventory[dest].append(newitem)
+            inventory[i][:] = []
+    
+    print_inventory(inventory)
+    print(inspectcount)
+    countmax = sorted(inspectcount)[-2:]
+    print(countmax[0] * countmax[1])
