9ff4bda1df814208aac45073c0b557743f026384
Matthew Zimmerman <matt@triclinic.org>
2023-01-22 22:37:05 -0500

    finished day 16, day 18

--- day16/day16.py
+++ day16/day16.py
@@ -20,15 +20,17 @@ class Valve:
             tunnels = m.group(3).split(", ")
             return cls(valve, flowrate, tunnels)
 
-Step = namedtuple("Step", "valve minute openvalves")
+Step = namedtuple("Step", "valve minute openvalves totalpressure")
 
 class ValveSeq:
     def __init__(self, valvelist):
         self.vlist = valvelist
         self.vindex = {}
+        self.bitm = {}
         self.dists = self._calc_dists()
-        for v in self.vlist:
+        for i, v in enumerate(self.vlist):
             self.vindex[v.name] = v
+            self.bitm[v.name] = 1 << i
         self.goodvalves = list(v.name for v in filter(lambda x: x.flowrate > 0, self.vlist))
         self.ngoodvalves = len(self.goodvalves)
     
@@ -63,9 +65,9 @@ class ValveSeq:
     def totalpressure(self, openvalves):
         return sum(self.vindex[k].flowrate * (30 - v) for k, v in openvalves.items())
     
-    def traverse(self, startvalve, debug=False):
+    def traverse(self, startvalve, totalminutes, debug=False):
         nsteps = 1
-        startstep = Step(startvalve, 1, {})
+        startstep = Step(startvalve, 1, {}, 0)
         queue = deque()
         queue.append(startstep)
         visited = set()
@@ -73,37 +75,40 @@ class ValveSeq:
         
         maxstep = None
         maxpressure = 0
-            
+        solutions = dict()
+
         while queue:
             s = queue.popleft()
             nsteps += 1
             if nsteps % 10000 == 0:
                 dig = (nsteps // 10000) % 10
                 print(dig, end="")
-            pressure = self.totalpressure(s.openvalves)
+            pressure = s.totalpressure
             if debug:
                 print(f"{pressure:5d} {s}")
-            if pressure > maxpressure:
-                maxpressure = pressure
-                maxstep = s
+            bitmask = sum(self.bitm[n] for n in s.openvalves.keys())
+            if bitmask not in solutions or solutions[bitmask].totalpressure < s.totalpressure:
+                solutions[bitmask] = s
             neighbors = []
             if s.valve not in s.openvalves:
                 # don't bother opening valves that don't release pressure
-                neighbors.append(Step(s.valve, s.minute+1, {**s.openvalves, s.valve: s.minute}))
+                pressure = (totalminutes - s.minute) * self.get(s.valve).flowrate
+                neighbors.append(Step(s.valve, s.minute+1, {**s.openvalves, s.valve: s.minute}, s.totalpressure + pressure))
             if len(s.openvalves) < self.ngoodvalves:
                 # only keep going down tunnels if there are still valves to be opened
                 for gv in self.goodvalves:
                     d = self.dist(s.valve, gv)
-                    if gv != s.valve and gv not in s.openvalves and s.minute+d <= 30:
-                        neighbors.append(Step(gv, s.minute+d, s.openvalves))
+                    if gv != s.valve and gv not in s.openvalves and s.minute+d <= totalminutes:
+                        neighbors.append(Step(gv, s.minute+d, s.openvalves, s.totalpressure))
             for ns in neighbors:
                 nstate = self.state(ns)
                 if nstate not in visited:
                     visited.add(nstate)
                     queue.append(ns)
-        print()
-        print(nsteps)
-        return maxpressure, maxstep
+
+        print(f"total steps={nsteps}")
+        print(len(solutions))
+        return solutions
     
     @classmethod
     def readfile(cls, filename):
@@ -120,9 +125,22 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     inp = ValveSeq.readfile(args.filename)
+    print("-- input data --")
     for v in inp.vlist:
         print(v)
+    indices = {v.name: 1 << i for i, v in enumerate(inp.vlist)}
+    print(indices)
     print()
-    print(inp.traverse("AA"), debug=args.d)
-    
-    
+
+    print("-- part 1 --")
+    sol1 = inp.traverse("AA", 30, debug=args.d)
+    #for k, v in sol1.items():
+    #    print(k, v)
+    print(max(v.totalpressure for k, v in sol1.items()))
+
+    print("-- part 2 --")
+    sol2 = inp.traverse("AA", 26)
+    print(max(sol2[k1].totalpressure + sol2[k2].totalpressure
+              for k1 in sol2.keys()
+              for k2 in sol2.keys()
+              if not k1 & k2))
---
+++ day18/__init__.py
@@ -0,0 +1,87 @@
+import argparse
+from collections import deque
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
+    z: int
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+
+def readfile(filename):
+    out = []
+    with open(filename, "r") as fh:
+        for l in fh:
+            out.append(Point(*(int(x) for x in l.strip().split(","))))
+    return out
+
+def inrange(p, ranges):
+    if p.x < ranges['xmin'] or p.x > ranges['xmax']:
+        return False
+    elif p.y < ranges['ymin'] or p.y > ranges['ymax']:
+        return False
+    elif p.z < ranges['zmin'] or p.z > ranges['zmax']:
+        return False
+    else:
+        return True
+
+def main(filename):
+    dirs = [
+        Point( 1,  0,  0),
+        Point(-1,  0,  0),
+        Point( 0,  1,  0),
+        Point( 0, -1,  0),
+        Point( 0,  0,  1),
+        Point( 0,  0, -1)]
+    points = set(readfile(filename))
+
+    print("-- part 1 ---")
+    area = 0
+    for p in points:
+        for d in dirs:
+            np = p + d
+            if np not in points:
+                area += 1
+    print(area)
+
+    ranges = {
+        'xmin': min(p.x for p in points)-1,
+        'xmax': max(p.x for p in points)+1,
+        'ymin': min(p.y for p in points)-1,
+        'ymax': max(p.y for p in points)+1,
+        'zmin': min(p.z for p in points)-1,
+        'zmax': max(p.z for p in points)+1}
+    print(ranges)
+
+    p0 = Point(ranges['xmin'], ranges['ymin'], ranges['zmin'])
+    queue = deque()
+    queue.append(p0)
+    visited = set()
+    visited.add(p0)
+
+    while queue:
+        p = queue.popleft()
+        for d in dirs:
+            np = p + d
+            if inrange(np, ranges) and np not in visited and np not in points:
+                queue.append(np)
+                visited.add(np)
+
+    area2 = 0
+    for p in points:
+        for d in dirs:
+            np = p + d
+            if np in visited:
+                area2 += 1
+    print(area2)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument('-d', action='store_true')
+    args = parser.parse_args()
+
+    main(args)
