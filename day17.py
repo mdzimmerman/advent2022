4af876c87f7e1901d47a170153e0ee45884b5a19
Matthew Zimmerman <matt@triclinic.org>
2023-01-24 23:32:12 -0500

    most of the way through day17

--- day17.py
+++ day17.py
@@ -21,6 +21,13 @@ def calcranges(cs):
         max(c.y for c in cs))
 
 
+def ymax(cs):
+    if cs:
+        return max(c.y for c in cs)
+    else:
+        return -1
+
+
 def move(s, dx, dy):
     return {Cell(c.x + dx, c.y + dy) for c in s}
 
@@ -28,7 +35,7 @@ def move(s, dx, dy):
 def printset(s, r=None):
     if r is None:
         r = calcranges(s)
-    print(r)
+    #print(r)
     for y in range(r.ymax, r.ymin - 1, -1):
         for x in range(r.xmin, r.xmax + 1):
             if Cell(x, y) in s:
@@ -38,16 +45,76 @@ def printset(s, r=None):
         print()
 
 
+def part1(wind, npieces=1, debug=0):
+    stopped = set()
+    i = 0
+    for j in range(npieces):
+        shape0 = SHAPES[j % len(SHAPES)]
+        shape = move(shape0, 2, ymax(stopped)+4)
+        if debug >= 2:
+            print()
+            printset(stopped | shape, Ranges(0, 6, 0, ymax(shape)))
+
+        inmotion = True
+        while inmotion:
+            # left or right
+            dir = wind[i % len(wind)]
+            canmove = True
+            if dir == "<":
+                for s in shape:
+                    sn = Cell(s.x-1, s.y)
+                    if sn.x < 0 or sn in stopped:
+                        canmove = False
+                if canmove:
+                    shape = move(shape, -1, 0)
+            elif dir == '>':
+                for s in shape:
+                    sn = Cell(s.x+1, s.y)
+                    if sn.x > 6 or sn in stopped:
+                        canmove = False
+                if canmove:
+                    shape = move(shape, 1, 0)
+            if debug >= 2:
+                print(dir)
+                printset(stopped | shape, Ranges(0, 6, 0, ymax(shape)))
+            i += 1
+
+            # down
+            for s in shape:
+                sn = Cell(s.x, s.y-1)
+                if sn.y < 0 or sn in stopped:
+                    inmotion = False
+            if inmotion:
+                shape = move(shape, 0, -1)
+            if debug >= 2:
+                print("v")
+                printset(stopped | shape, Ranges(0, 6, 0, ymax(shape)))
+
+        stopped.update(shape)
+
+    if debug >= 1:
+        print()
+        printset(stopped, Ranges(0, 6, 0, ymax(stopped)))
+
+    return ymax(stopped)+1
+
 def main():
-    print("-- part 1 --")
+    print("-- shapes --")
 
     for s in SHAPES:
         print()
-        sm = move(s, 2, 3)
-        print(sm)
-        r = calcranges(sm)
-        printset(sm, Ranges(0, 6, 0, r.ymax))
+        print(s)
+        printset(s)
 
+    print()
+    print("-- part 1 --")
+    print("test")
+    test = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
+    #part1(test, 2)
+    print(part1(test, npieces=5, debug=1))
+
+    print()
+    print(part1(test, npieces=2022, debug=0))
 
 if __name__ == '__main__':
     main()
