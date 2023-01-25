875b9b724f2036dde74302966d6a2252b8740563
Matthew Zimmerman <matt@triclinic.org>
2023-01-25 08:51:38 -0500

    finished day 17 part 1

--- day17.py
+++ day17.py
@@ -45,10 +45,39 @@ def printset(s, r=None):
         print()
 
 
-def part1(wind, npieces=1, debug=0):
+def canmove(dir, shape, stopped):
+    if dir == '<':
+        for s in shape:
+            sn = Cell(s.x-1, s.y)
+            if sn.x < 0 or sn in stopped:
+                return False, shape
+        return True, move(shape, -1, 0)
+    elif dir == '>':
+        for s in shape:
+            sn = Cell(s.x+1, s.y)
+            if sn.x > 6 or sn in stopped:
+                return False, shape
+        return True, move(shape, 1, 0)
+    elif dir == 'v':
+        for s in shape:
+            sn = Cell(s.x, s.y-1)
+            if sn.y < 0 or sn in stopped:
+                return False, shape
+        return True, move(shape, 0, -1)
+
+
+def prune(stopped, buffer=50):
+    y1 = ymax(stopped)
+    y0 = y1 - buffer
+    return set(filter(lambda c: c.y >= y0, stopped))
+
+
+def simulate(wind, npieces=1, debug=0):
     stopped = set()
     i = 0
     for j in range(npieces):
+        #if j % 1000 == 0:
+        #    print(f"j={j}")
         shape0 = SHAPES[j % len(SHAPES)]
         shape = move(shape0, 2, ymax(stopped)+4)
         if debug >= 2:
@@ -59,38 +88,19 @@ def part1(wind, npieces=1, debug=0):
         while inmotion:
             # left or right
             dir = wind[i % len(wind)]
-            canmove = True
-            if dir == "<":
-                for s in shape:
-                    sn = Cell(s.x-1, s.y)
-                    if sn.x < 0 or sn in stopped:
-                        canmove = False
-                if canmove:
-                    shape = move(shape, -1, 0)
-            elif dir == '>':
-                for s in shape:
-                    sn = Cell(s.x+1, s.y)
-                    if sn.x > 6 or sn in stopped:
-                        canmove = False
-                if canmove:
-                    shape = move(shape, 1, 0)
+            _, shape = canmove(dir, shape, stopped)
             if debug >= 2:
                 print(dir)
                 printset(stopped | shape, Ranges(0, 6, 0, ymax(shape)))
             i += 1
 
             # down
-            for s in shape:
-                sn = Cell(s.x, s.y-1)
-                if sn.y < 0 or sn in stopped:
-                    inmotion = False
-            if inmotion:
-                shape = move(shape, 0, -1)
+            inmotion, shape = canmove('v', shape, stopped)
             if debug >= 2:
                 print("v")
                 printset(stopped | shape, Ranges(0, 6, 0, ymax(shape)))
 
-        stopped.update(shape)
+        stopped = prune(stopped | shape, buffer=30)
 
     if debug >= 1:
         print()
@@ -108,13 +118,30 @@ def main():
 
     print()
     print("-- part 1 --")
-    print("test")
+    print("- test - ")
     test = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
     #part1(test, 2)
-    print(part1(test, npieces=5, debug=1))
+    print("test with 5 blocks")
+    print(simulate(test, npieces=5, debug=1))
 
     print()
-    print(part1(test, npieces=2022, debug=0))
+    print(simulate(test, npieces=2022, debug=0))
+
+    print("- input -")
+    inp = ""
+    with open("day17/input.txt", "r") as fh:
+        for l in fh:
+            inp += l.strip()
+    #print(inp)
+    print(simulate(inp, npieces=2022))
+
+    print("-- part 2 --")
+    print("- test -")
+    # blergh too much to simulate
+    #print(simulate(test, npieces=1000000000000))
+    print(f"test length={len(test)}")
+    print(f"inp length={len(inp)}")
+
 
 if __name__ == '__main__':
     main()
